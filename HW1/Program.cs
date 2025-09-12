c16d3171a0fa1c65d2ed1ac2694ee87aaa506897
nhatquang-jinaz <nhatquang10messi@gmail.com>
2025-09-12 07:39:51 +0700

    Update Program.cs

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HW_
+namespace HW1
 {
     internal class Program
     {
@@ -24,3 +24,4 @@ namespace HW_
         }
     }
 }
+
