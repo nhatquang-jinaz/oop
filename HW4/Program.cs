939161035a4f33e71274f96ae39761b9f619776a
nhatquang-jinaz <nhatquang10messi@gmail.com>
2025-10-02 19:03:50 +0700

    Nop bai HW4

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -1,10 +1,46 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
 {
-    
-    private static void Main(string[] args)
+    internal class Program
     {
-        MyDate md1 = new MyDate(DateTime.Now.Day,DateTime.Now.Month, DateTime.Now.Year);
-        Console.WriteLine("{0}/{1}/{2}",md1.Day,md1.Month,md1.Year);
-        Console.ReadKey();
+        static void Main()
+        {
+            Console.InputEncoding = Encoding.UTF8;
+            Console.OutputEncoding = Encoding.UTF8;
+            List<ThueXe> ds = new List<ThueXe>();
+
+            Console.Write("Nhập số lượng người thuê: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"--- Người thuê {i + 1} ---");
+                Console.Write("Họ tên: ");
+                string ten = Console.ReadLine();
+
+                Console.Write("Số giờ thuê: ");
+                int gio = int.Parse(Console.ReadLine());
+
+                Console.Write("Loại xe (1. Xe du lịch, 2. Xe tải): ");
+                int loai = int.Parse(Console.ReadLine());
+
+                if (loai == 1)
+                    ds.Add(new XeDuLich(ten, gio));
+                else
+                    ds.Add(new XeTai(ten, gio));
+            }
+
+            Console.WriteLine("\nDanh sách thuê xe:");
+            Console.WriteLine("Họ tên\tSố giờ\tThành tiền");
+            foreach (var xe in ds)
+            {
+                Console.WriteLine($"{xe.HoTen}\t{xe.SoGio}\t{xe.TinhTien()}");
+            }
+        }
     }
 }
---
+++ HW4/Thuexe.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4
+{
+    class ThueXe
+    {
+        public string HoTen { get; set; }
+        public int SoGio { get; set; }
+
+        public ThueXe(string hoTen, int soGio)
+        {
+            HoTen = hoTen;
+            SoGio = soGio;
+        }
+
+        public virtual int TinhTien()
+        {
+            return 0;
+        }
+    }
+
+    class XeDuLich : ThueXe
+    {
+        public XeDuLich(string hoTen, int soGio) : base(hoTen, soGio) { }
+
+        public override int TinhTien()
+        {
+            if (SoGio <= 1) return 250000;
+            return 250000 + (SoGio - 1) * 70000;
+        }
+    }
+
+    class XeTai : ThueXe
+    {
+        public XeTai(string hoTen, int soGio) : base(hoTen, soGio) { }
+
+        public override int TinhTien()
+        {
+            if (SoGio <= 1) return 220000;
+            return 220000 + (SoGio - 1) * 85000;
+        }
+    }
+
+}
